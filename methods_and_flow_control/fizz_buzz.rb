eb8b31dafb9778e9118390a91f989e785d042a7e
Zach Langer <zacho19@gmail.com>
2014-07-28 14:36:15 -0600

    git blog post URL: https://zachlangerruby.wordpress.com/2014/06/24/git-basics/

---
+++ methods_and_flow_control/fizz_buzz.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+number = 1
+while number != 101
+  if number % 3 == 0 && number % 5 == 0
+    puts 'FizzBuzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  else
+    puts number
+  end
+  number += 1
+end
---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+sentance = 'this is a string to practice with'
+puts sentance
+puts sentance.capitalize
+puts sentance.upcase
+puts sentance.gsub('string', "'string'")
+puts 'the string ' + sentance + ' has ' + "#{sentance.length}" + ' characters'
+puts sentance.reverse
+puts sentance.slice(20..28) * 2 + sentance.slice(20..27) + '!'
